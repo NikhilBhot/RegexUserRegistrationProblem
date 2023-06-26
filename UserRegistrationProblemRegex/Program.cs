65d7c0c8b7ae667cdef0832ef0782727e4d25cf7
unknown <bhotnikhil25@gmail.com>
2023-06-26 21:06:35 +0530

    [Nikhil]-Validate last Name Using Regex

--- UserRegistrationProblemRegex/Program.cs
+++ UserRegistrationProblemRegex/Program.cs
@@ -21,16 +21,41 @@ namespace UserRegistrationProblemRegex
             }
 
             Console.ReadLine();
+
+            Console.WriteLine("Enter your last name:");
+            string lastName = Console.ReadLine();
+
+            bool isValidLastName = ValidateLastName(lastName);
+
+            if (isValidLastName)
+            {
+                Console.WriteLine("Valid last name.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid last name. Last name should start with a capital letter and have a minimum of 3 characters.");
+            }
+
+            Console.ReadLine();
         }
+    
 
 
-        /*As a User need to enter a valid FirstName
-           - First name starts with Cap and has minimum 3 characters
-         */
-        static bool ValidateFirstName(string firstName)
-        {
-            string pattern = "^[A-Z][a-zA-Z]{2,}$";
-            return Regex.IsMatch(firstName, pattern);
-        }
+    /*As a User need to enter a valid FirstName
+       - First name starts with Cap and has minimum 3 characters
+     */
+    static bool ValidateFirstName(string firstName)
+    {
+        string pattern = "^[A-Z][a-zA-Z]{2,}$";
+        return Regex.IsMatch(firstName, pattern);
     }
+
+        /*As a User need to enter a valid Last Name - Last name starts with Cap and has minimum 3 characters
+         */
+        static bool ValidateLastName(string lastName)
+    {
+        string pattern = "^[A-Z][a-zA-Z]{2,}$";
+        return Regex.IsMatch(lastName, pattern);
+    }
+}
 }
